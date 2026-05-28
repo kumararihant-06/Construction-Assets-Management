414830cde88c0fccbd37f5ab869fe6b68c7770d8
Arihant Kumar <kumararihant.iiitm@gmail.com>
2026-05-29 01:25:26 +0530

    chore: configure SQLite connection and register DbContext.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using ConstructionAssetAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder  =  WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidProgramException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => new [] {"hello"});
